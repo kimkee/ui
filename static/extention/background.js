546a2812ce7d2d640c1412669c40e1840aa292bc
KimKeeHyun <kimkee7@gmail.com>
2022-07-17 21:12:46 +0900

    extention

---
+++ static/extention/background.js
@@ -0,0 +1,21 @@
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		files: ['content.js']
+	});
+});
+console.log("bacground.js");
+
+
+function reddenPage() {
+	document.body.style.backgroundColor = 'red';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+	if (!tab.url.includes("chrome://")) {
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			function: reddenPage
+		});
+	}
+});
