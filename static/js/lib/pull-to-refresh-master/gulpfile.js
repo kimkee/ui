f889342bd5afa204f81c4941f05f0db20d8d941e
KimKeeHyun <kimkee7@gmail.com>
2025-01-21 14:35:44 +0900

    pull-to-refresh-master   delete

--- static/js/lib/pull-to-refresh-master/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-
-gulp.task('default', () => gulp.src('src/**/*.css')
-  .pipe(postcss([autoprefixer()]))
-  .pipe(gulp.dest('dist'))
-)
